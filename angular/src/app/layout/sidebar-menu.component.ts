f6b654e258d3991dbbaf659c9d64e9ab1b4f1fdd
MinhTuyen <tuyen.nguyen@yedda.io>
2023-04-13 12:06:54 +0700

    p

--- angular/src/app/layout/sidebar-menu.component.ts
+++ angular/src/app/layout/sidebar-menu.component.ts
@@ -44,7 +44,7 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
     getMenuItems(): MenuItem[] {
         return [
             // new MenuItem(this.l('About'), '/app/about', 'fas fa-info-circle'),
-            // new MenuItem(this.l('HomePage'), '/app/home', 'fas fa-home'),
+            new MenuItem(this.l('HomePage'), '/app/home', 'fas fa-home'),
             new MenuItem(
                 this.l('Roles'),
                 '/app/roles',
@@ -102,7 +102,7 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
                     'far fa-circle',
                     '',
                 ),
-                
+
             ])
         ];
     }
